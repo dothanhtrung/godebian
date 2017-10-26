8f521a38c25318694c0ff441891862e1fc6b263c
kimtinh <onlyesterday.16@gmail.com>
2017-10-26 17:48:51 +0700

    Handle EOF

--- deb822/deb822.go
+++ deb822/deb822.go
@@ -32,8 +32,9 @@ func find(pathfile, skey, svalue string, limit uint) ([]map[string]string, error
 	var count uint
 	for scanner.Scan() {
 		line := scanner.Text()
-		// Break between sections
+
 		if ignore {
+			// Break between sections
 			breakMatch, _ := regexp.MatchString("^\\s*$", line)
 			if breakMatch {
 				ignore = false
@@ -41,46 +42,89 @@ func find(pathfile, skey, svalue string, limit uint) ([]map[string]string, error
 			continue
 		}
 
-		keyMatch, _ := regexp.MatchString("^\\S*:.*", line)
+		/* |KEY1:           ----> this line
+		 * |  blabla
+		 * |    blablo
+		 * |KEY2: bleble    ----> this line
+		 * |
+		 * |KEY1: bloblo    ----> this line
+		 */
+		keyMatch, _ := regexp.MatchString("^\\S+:.*", line)
 		if keyMatch {
-			if (key == skey || skey == "") && (value == svalue || svalue == "") {
-				found = true
-			} else if key != "" && key == skey && value != svalue {
-				ignore = true
-				section = make(map[string]string)
-				key = ""
-				value = ""
+			if key != "" {
+				section[key] = value
+				if (key == skey || skey == "") && (value == svalue || svalue == "") {
+					found = true
+				} else if key == skey && value != svalue {
+					ignore = true
+					section = make(map[string]string)
+					key, value = "", ""
+				}
+			}
+			kv := strings.Split(line, ":")
+			key = strings.TrimSpace(kv[0])
+			value = strings.TrimSpace(kv[1])
+			continue
+		}
+
+		/* |KEY1:
+		 * |  blabla        ----> this line
+		 * |    blablo      ----> this line
+		 * |KEY2: bleble
+		 * |
+		 * |KEY1: bloblo
+		 */
+		valueMatch, _ := regexp.MatchString("^\\s+\\S+", line)
+		if valueMatch {
+			if value != "" {
+				value += "\n"
+			}
+			value += strings.TrimSpace(line)
+			continue
+		}
+
+		/* |KEY1:
+		 * |  blabla
+		 * |    blablo
+		 * |KEY2: bleble
+		 * |                ----> this line
+		 * |KEY1: bloblo
+		 */
+		breakMatch, _ := regexp.MatchString("^\\s*$", line)
+		if breakMatch {
+			if key == "" {
 				continue
 			}
 
 			section[key] = value
-			kv := strings.Split(line, ":")
-			key = strings.TrimSpace(kv[0])
-			value = strings.TrimSpace(kv[1])
-			section[key] = value
-		} else {
-			valueMatch, _ := regexp.MatchString("^\\s+\\S*", line)
-			if valueMatch {
-				if value != "" {
-					value += "\n"
-				}
-				value += strings.TrimSpace(line)
-			} else {
-				breakMatch, _ := regexp.MatchString("^\\s*$", line)
-				if breakMatch {
-					if found {
-						results = append(results, section)
-						count++
-						found = false
-						if limit > 0 && count >= limit {
-							break
-						}
-					}
 
-					section = make(map[string]string)
-					key = ""
-					value = ""
+			if (key == skey || skey == "") && (value == svalue || svalue == "") {
+				found = true
+			}
+
+			if found {
+				results = append(results, section)
+				count++
+				found = false
+				if limit > 0 && count >= limit {
+					break
 				}
+			}
+
+			section = make(map[string]string)
+			key = ""
+			value = ""
+			continue
+		}
+
+	}
+
+	// At the EOF
+	if key != "" {
+		section[key] = value
+		if (key == skey || skey == "") && (value == svalue || svalue == "") {
+			if limit == 0 || count < limit {
+				results = append(results, section)
 			}
 		}
 	}
